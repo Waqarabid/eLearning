d24fa21d796a61525d1773a990fd0d5beb20d742
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-03-15 00:35:55 +0530

    Configure Express app && Setup Routes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
-console.log("Server is running");
+import express from "express";
+import cors from "cors";
+import { readdirSync } from "fs";
+const morgan = require("morgan");
+require("dotenv").config();
+
+// create express app
+const app = express();
+
+// apply middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// routes
+readdirSync("./routes").map((routesFile) =>
+  app.use("/api", require(`./routes/${routesFile}`))
+);
+
+// port
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
