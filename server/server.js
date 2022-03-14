7abf2e87f2c14caaeab64ed29ba7be0a9c143188
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-03-15 01:44:11 +0530

    Mongoose connect

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 import express from "express";
 import cors from "cors";
 import { readdirSync } from "fs";
+import mongoose from "mongoose";
 const morgan = require("morgan");
 require("dotenv").config();
 
 // create express app
 const app = express();
+
+// db
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("**DB connected**"))
+  .catch((err) => console.log("DB CONNECTION ERROR: ", err));
 
 // apply middlewares
 app.use(cors());
