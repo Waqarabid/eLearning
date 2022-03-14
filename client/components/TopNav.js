0b15420376d057b4f0752b57ed61409f91f050c2
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-03-14 23:02:09 +0530

    Ant menu and icons

--- client/components/TopNav.js
+++ client/components/TopNav.js
@@ -1,8 +1,34 @@
+import { Menu } from "antd";
+import Link from "next/link";
+import {
+  AppstoreAddOutlined,
+  LoginOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
+
+const { Item } = Menu;
+
 const TopNav = () => {
   return (
-    <>
-      <p>TopNav</p>
-    </>
+    <Menu mode="horizontal">
+      <Item icon={<AppstoreAddOutlined />}>
+        <Link href="/">
+          <a>App</a>
+        </Link>
+      </Item>
+
+      <Item icon={<LoginOutlined />}>
+        <Link href="/login">
+          <a>Login</a>
+        </Link>
+      </Item>
+
+      <Item icon={<UserAddOutlined />}>
+        <Link href="/register">
+          <a>Register</a>
+        </Link>
+      </Item>
+    </Menu>
   );
 };
 
