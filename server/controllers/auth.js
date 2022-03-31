952b2a4610cffc6ea563059f6879163e3a76fb64
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-03-31 18:41:30 +0530

    Verify token and get current user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -70,3 +70,14 @@ export const logout = async (req, res) => {
     console.log(err);
   }
 };
+
+export const currentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password").exec();
+    console.log("CURRENT_USER", user);
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
+  }
+};
