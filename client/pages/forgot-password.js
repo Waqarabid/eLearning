75f55acf6cca0a43ff70ae63304a8b788aec5c5f
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-04-03 15:43:08 +0530

    Forgot password page

---
+++ client/pages/forgot-password.js
@@ -0,0 +1,77 @@
+import { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+
+const ForgotPassword = () => {
+  // state
+  const [email, setEmail] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [code, setCode] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // context
+  const {
+    state: { user },
+  } = useContext(Context);
+
+  // router
+  const router = useRouter();
+
+  // redirect if logged in
+  useEffect(() => {
+    if (user !== null) {
+      router.push("/");
+    }
+  }, [user]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.table({ name, email, password });
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`/api/forgot-password`, {
+        email,
+      });
+      setSuccess(true);
+      toast("Check your email for a reset code");
+    } catch (err) {
+      toast.error(err.response.data);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">
+        Forgot Password
+      </h1>
+
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            className="form-control mb-4 p-4 border-0"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            className="btn btn-primary btn-block mb-4 p-4"
+            disabled={loading || !email}
+          >
+            {loading ? <SyncOutlined spin /> : "Send Reset Code"}
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ForgotPassword;
