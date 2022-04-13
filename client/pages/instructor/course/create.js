8d3b3efbea83e329a2a6c3212a1fb99297e0e445
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-04-13 23:13:43 +0530

    Create course form

--- client/pages/instructor/course/create.js
+++ client/pages/instructor/course/create.js
@@ -1,10 +1,42 @@
-import axios from "axios";
+import { useState, useEffect } from "react";
 import InstructorRoute from "../../../components/routes/InstructorRoute";
 
+import CourseCreateForm from "../../../components/forms/CourseCreateForm";
+
 const CourseCreate = () => {
+  // state
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "9.99",
+    uploading: false,
+    paid: true,
+    imagePreview: "",
+  });
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleImage = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
   return (
     <InstructorRoute>
       <h1 className="jumbotron text-center square">Create Course</h1>
+      <div className="pt-3 pb-3">
+        <CourseCreateForm
+          handleSubmit={handleSubmit}
+          handleImage={handleImage}
+          handleChange={handleChange}
+          values={values}
+          setValues={setValues}
+        />
+      </div>
     </InstructorRoute>
   );
 };
