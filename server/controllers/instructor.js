e90209a7b58651aa756bc4b1bb63a4a289ae7961
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2023-07-01 18:33:00 +0530

    Instructor courses in dashboard

--- server/controllers/instructor.js
+++ server/controllers/instructor.js
@@ -1,4 +1,5 @@
 import User from "../models/user";
+import Course from "../models/course";
 import queryString from "query-string";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -69,6 +70,18 @@ export const currentInstructor = async (req, res) => {
     } else {
       res.json({ ok: true });
     }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// instructorCourses finds courses by the current instructor
+export const instructorCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({ instructor: req.user._id })
+      .sort({ createdAt: -1 })
+      .exec();
+    res.json(courses);
   } catch (err) {
     console.log(err);
   }
